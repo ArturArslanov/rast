b8a70b7360d652f65d66ece0348f266bdff6940e
Artur <aat281205@gmail.com>
2022-03-01 20:15:09 +0300

    fourth commit

--- data/api_jobs.py
+++ data/api_jobs.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+from json import loads
+
 import flask
-from flask import jsonify
+from flask import jsonify, request
 
 from . import db_session
 from .jobs import Jobs
+from .users import User
 
 blueprint = flask.Blueprint(
     'api_jobs',
@@ -37,3 +41,28 @@ def get_one_news(jobs_id):
                 'collaborators', 'is_finished'))
         }
     )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_news():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in ['job', 'work_size', 'collaborators',
+                                                 'is_finished', 'user_id']):
+        return jsonify({'error': 'Bad request'})
+    res = request.json
+    session = db_session.create_session()
+    user = session.query(User).filter(User.id == res['user_id']).first()
+    job = Jobs()
+    job.job = res['job']
+    job.work_size = res['work_size']
+    job.collaborators = res['collaborators']
+    job.is_finished = res['is_finished']
+    if res['is_finished']:
+        if not res['datetime']:
+            job.end_date = datetime.today()
+        else:
+            job.end_date = res['datetime']
+    user.jobs.append(job)
+    session.commit()
+    return jsonify({'success': 'OK'})
--- test.py
+++ test.py
@@ -8,3 +8,8 @@ req3 = get('http://127.0.0.2:8081/api/jobs/91239')
 print(req3.json())
 req4 = get('http://127.0.0.2:8081/api/jobs/ert')
 print(req4)
+
+req5 = post('http://127.0.0.2:8081/api/jobs',
+            json={'job': 'real_job2', 'work_size': 20, 'collaborators': '1, 2',
+                  'is_finished': False, 'user_id': 1})
+print(req5)
