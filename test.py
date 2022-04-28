873dfa6b8c3925852eb681cefe6c6f204b70d0ee
Artur <aat281205@gmail.com>
2022-04-28 21:11:15 +0300

    change

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 from requests import get, post, delete, put
 
 # удаление
-# req8 = delete('http://127.0.0.2:8081/api/jobs/95')  # удалит
-# print(req8.json())
-#
-# req1 = delete('http://127.0.0.2:8081/api/jobs/9')  # работы с таким айди не существует
-# print(req1.json())
-#
-# req2 = get('http://127.0.0.2:8081/api/jobs/96')  # метод get
-# print(req2.json())
+req8 = delete('http://127.0.0.2:8081/api/jobs/95')  # удалит
+print(req8.json())
+
+req1 = delete('http://127.0.0.2:8081/api/jobs/9')  # работы с таким айди не существует
+print(req1.json())
+
+req2 = get('http://127.0.0.2:8081/api/jobs/96')  # метод get
+print(req2.json())
 
 # изменение
 req3 = get('http://127.0.0.2:8081/api/jobs')
